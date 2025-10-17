8c20974dba9b0052fde141724bf4b3c1460149b7
Ronald Rengifo <ronaldfaridrengifo@gmail.com>
2025-10-16 21:37:24 -0500

    tercer commit

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Word");
         System.out.println("Modificacion para nueva rama");
+        System.out.println("tercer funcionalidad");
     }
 }
